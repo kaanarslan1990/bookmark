df41fd48e5377f3affe59604e48e67080f74bc6b
kaanarslan1990 <oguzkaanarslan@outlook.com>
2022-10-14 15:56:00 +0300

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     screens: {
-      sm:'480px',
-      md: '760px',
+      sm: '480px',
+      md: '768px',
       lg: '1020px',
       xl: '1440px',
     },
